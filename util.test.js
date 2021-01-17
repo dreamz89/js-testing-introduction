7b408fa797e297f79c07141197ae9d0d1b8bc440
Jocelyn Tan <jocelyntan89@hotmail.com>
2021-01-17 22:08:10 +0100

    added unit test

---
+++ util.test.js
@@ -0,0 +1,10 @@
+const { TestScheduler } = require('jest')
+const { generateText } = require('./util')
+
+test('should output name and age', () => {
+  const text = generateText('Max', 29)
+  expect(text).toBe('Max (29 years old)')
+
+  const text2 = generateText('Anna', 28)
+  expect(text2).toBe('Anna (28 years old)')
+})
