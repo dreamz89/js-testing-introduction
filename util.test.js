dda6248c7955a39567e8ea6099270fe4a043fbe4
Jocelyn Tan <jocelyntan89@hotmail.com>
2021-02-01 17:20:06 +0100

    add integration test

--- util.test.js
+++ util.test.js
@@ -1,5 +1,5 @@
 const { TestScheduler } = require('jest')
-const { generateText } = require('./util')
+const { generateText, checkAndGenerate } = require('./util')
 
 test('should output name and age', () => {
   const text = generateText('Max', 29)
@@ -7,4 +7,9 @@ test('should output name and age', () => {
 
   const text2 = generateText('Anna', 28)
   expect(text2).toBe('Anna (28 years old)')
+})
+
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate('Max', 29)
+  expect(text).toBe('Max (29 years old)')
 })
